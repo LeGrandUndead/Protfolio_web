bd73e35bd11508584c5a3504f5c658c8744b4d1a
LeGrandUndead <kevinmowilliam@gmail.com>
2026-04-26 15:29:33 -0500

    Add GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
-  plugins: [],
+  plugins: [react()],
+  base: './',
 };
